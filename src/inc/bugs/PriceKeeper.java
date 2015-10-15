bafc3c6437d6580fde979b3b214c002fc6b68e1d
Pedro Janeiro <pedro_janeirito@hotmail.com>
2015-10-15 16:47:40 +0000

    Price Requester connects to Price Keeper. Price Requester asks for search terms. There's somthing wrong with the answer received, though.

--- src/inc/bugs/PriceKeeper.java
+++ src/inc/bugs/PriceKeeper.java
@@ -21,6 +21,9 @@ import java.util.concurrent.ConcurrentSkipListSet;
 public class PriceKeeper {
     private ConcurrentSkipListSet<Smartphone> smartphones = null;
     private TopicListener topicListener = null;
+    private QueueListener queueListener = null;
+    private static String xsdURL;
+
     /**
      * Price Keeper
      *
@@ -32,14 +35,24 @@ public class PriceKeeper {
      * @throws JMSException
      * @throws NamingException
      */
-    public static void main(String[] args) throws JMSException, NamingException, JAXBException {
+    public static void main(String[] args) throws JMSException, NamingException, JAXBException, InterruptedException {
+        if (args.length == 1) {
+            xsdURL = args[0];
+        } else {
+            System.err.println("Invalid number of arguments.\nSyntax: PriceKeeper &lt;xsd&gt;");
+        }
         PriceKeeper priceKeeper = new PriceKeeper();
     }
 
-    public PriceKeeper() throws JMSException, NamingException, JAXBException {
+    public PriceKeeper() throws JMSException, NamingException, JAXBException, InterruptedException {
         this.smartphones = new ConcurrentSkipListSet<Smartphone>();
         this.topicListener = new TopicListener(this);
+        this.queueListener = new QueueListener(this);
         this.topicListener.start();
+        this.queueListener.start();
+        this.topicListener.join();
+        this.queueListener.join();
+
     }
 
     public void updatePrices(ArrayList<String> arrayList) throws JAXBException {
@@ -60,8 +73,9 @@ public class PriceKeeper {
                 }
             }
         } else if(searchTerms.containsKey(SEARCH_MODES.MARCA_MODELO)) {
+            String[] marcaModelo = searchTerms.get(SEARCH_MODES.MARCA_MODELO).trim().split("-|\\s+");
             for(Smartphone current : smartphones) {
-                if(current.getBrand().compareToIgnoreCase(searchTerms.get(SEARCH_MODES.MARCA_MODELO)) == 0 && current.getName().toUpperCase().contains(searchTerms.get(SEARCH_MODES.MARCA_MODELO).toUpperCase())) {
+                if(current.getBrand().toUpperCase().compareTo(marcaModelo[0].toUpperCase()) == 0 && current.getName().toUpperCase().contains(marcaModelo[1].toUpperCase())) {
                     result.add(current);
                 }
             }
@@ -72,7 +86,7 @@ public class PriceKeeper {
                 }
             }
         } else if(searchTerms.containsKey(SEARCH_MODES.INTERVALO_PRECOS)) {
-            String[] preco = searchTerms.get(SEARCH_MODES.INTERVALO_PRECOS).split(" ");
+            String[] preco = searchTerms.get(SEARCH_MODES.INTERVALO_PRECOS).trim().split("-|\\s+");
             for(Smartphone current : smartphones) {
                 if(current.getPrice().doubleValue()>=Double.parseDouble(preco[0]) && current.getPrice().doubleValue()<=Double.parseDouble(preco[1])) {
                     result.add(current);
@@ -94,7 +108,27 @@ public class PriceKeeper {
         public TopicListener(PriceKeeper priceKeeper) throws JMSException, NamingException, JAXBException {
             super();
             this.priceKeeper = priceKeeper;
-            this.initialize();
+        }
+
+        @Override
+        public void run(){
+            try {
+                this.initialize();
+            } catch (JMSException|NamingException e) {
+                System.err.println("Error creating connection to topic.");
+                System.exit(1);
+            }
+            System.out.println("Begin listening to topic");
+            while (true) {
+                try {
+                    getPrices();
+                } catch (JMSException|NamingException|JAXBException e) {
+                    System.err.println("Error getting prices from topic.");
+                }
+            }
+        }
+
+        public void initialize() throws JMSException, NamingException {
             Runtime.getRuntime().addShutdownHook(new Thread() {
                 public void run() {
                     try {
@@ -106,26 +140,6 @@ public class PriceKeeper {
                     }
                 }
             });
-
-        }
-
-        @Override
-        public void run(){
-            while (true) {
-                try {
-                    getPrices();
-                } catch (JMSException|NamingException|JAXBException e) {
-                    System.err.println("Error getting prices from topic.");
-                }
-            }
-        }
-
-        @Override
-        public void start() {
-            System.out.println("Begin listening to topic");
-        }
-
-        public void initialize() throws JMSException, NamingException {
             System.setProperty("java.naming.factory.initial","org.jboss.naming.remote.client.InitialContextFactory");
             System.setProperty("java.naming.provider.url","http-remoting://localhost:8080");
             this.topicConnectionFactory = InitialContext.doLookup("jms/RemoteConnectionFactory");
@@ -146,6 +160,14 @@ public class PriceKeeper {
             } else {
                 ArrayList<String> result = new ArrayList<String>();
                 result = objectMessage.getBody(result.getClass());
+                System.out.println("Evaluating data validity");
+                XMLValidator xmlValidator = new XMLValidator();
+                for(String xml : result) {
+                    if(!xmlValidator.isValidXML(xml, xsdURL)) {
+                        System.err.println("Invalid XML. Ignorig received values.");
+                        return;
+                    }
+                }
                 System.out.println("Prices received");
                 this.priceKeeper.updatePrices(result);
             }
@@ -160,31 +182,38 @@ public class PriceKeeper {
         public QueueListener(PriceKeeper priceKeeper) throws JMSException, NamingException, JAXBException {
             super();
             this.priceKeeper = priceKeeper;
-            this.initialize();
-        }
-
-        @Override
-        public void start() {
-            System.out.println("Begin listening to queue");
         }
 
         @Override
         public void run() {
+            try {
+                this.initialize();
+            } catch (JMSException|NamingException e) {
+                System.err.println("Error creating connection to queue.");
+                System.exit(1);
+            }
+            System.out.println("Begin listening to queue");
             while (true) {
-                HashMap<SEARCH_MODES,String> hashMap = null;
+                HashMap<SEARCH_MODES,String> hashMap = new HashMap<SEARCH_MODES,String>();
                 Destination replyTo = null;
                 try(JMSContext jmsContext = connectionFactory.createContext("pjaneiro","|Sisc00l")){
                     JMSConsumer consumer = jmsContext.createConsumer(destination);
                     Message message = consumer.receive();
                     replyTo = message.getJMSReplyTo();
                     hashMap = message.getBody(hashMap.getClass());
-                    ArrayList<Smartphone> searchResult = this.priceKeeper.search(hashMap);
-
                 }
                 catch (JMSException e) {
                     System.err.println("Error receiving request.");
+                    continue;
                 }
-
+                ArrayList<Smartphone> searchResult = this.priceKeeper.search(hashMap);
+                try (JMSContext jmsContext = connectionFactory.createContext("pjaneiro","|Sisc00l")){
+                    ObjectMessage objectMessage = jmsContext.createObjectMessage(searchResult);
+                    JMSProducer jmsProducer = jmsContext.createProducer();
+                    jmsProducer.send(replyTo, objectMessage);
+                } catch (JMSRuntimeException e) {
+                    System.err.println("Error sending answer.");
+                }
             }
         }
 
---
+++ src/inc/bugs/PriceRequester.java
@@ -0,0 +1,157 @@
+package inc.bugs;
+
+import generated.Smartphone;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * Created by pedro on 15-10-2015.
+ */
+public class PriceRequester {
+    private ConnectionFactory connectionFactory = null;
+    private Session session = null;
+    private Connection connection = null;
+    private Destination destination = null;
+    private TemporaryQueue temporaryQueue = null;
+
+    public static void main(String[] args) throws JMSException, NamingException, IOException {
+        PriceRequester priceRequester = new PriceRequester();
+        priceRequester.mainLoop();
+    }
+
+    public PriceRequester() throws JMSException, NamingException {
+        this.initialize();
+    }
+
+    private void initialize() throws JMSException, NamingException {
+        System.setProperty("java.naming.factory.initial","org.jboss.naming.remote.client.InitialContextFactory");
+        System.setProperty("java.naming.provider.url","http-remoting://localhost:8080");
+        this.connectionFactory = InitialContext.doLookup("jms/RemoteConnectionFactory");
+        this.destination = InitialContext.doLookup("jms/queue/queue");
+    }
+
+    private ArrayList<Smartphone> requestInfo(HashMap<SEARCH_MODES, String> searchTerms) {
+        ArrayList<Smartphone> result = new ArrayList<Smartphone>();
+        System.out.println("Sending request.");
+        try {
+            connection = connectionFactory.createConnection("pjaneiro", "|Sisc00l");
+            connection.start();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            temporaryQueue = session.createTemporaryQueue();
+            ObjectMessage objectMessage = session.createObjectMessage(searchTerms);
+            objectMessage.setJMSReplyTo(temporaryQueue);
+            session.createProducer(destination).send(objectMessage);
+        } catch (JMSException e) {
+            System.err.println("Error sending request.");
+            return null;
+        }
+        System.out.println("Waiting for response.");
+        try {
+            MessageConsumer messageConsumer = session.createConsumer(temporaryQueue);
+            Message message = messageConsumer.receive();
+            result = message.getBody(result.getClass());
+            temporaryQueue.delete();
+            connection.stop();
+            session.close();
+            connection.close();
+        } catch (JMSException e) {
+            System.err.println("Error receiving answer.");
+            return null;
+        }
+        return  result;
+    }
+
+    private void mainLoop() throws IOException {
+        HashMap<SEARCH_MODES, String> searchTerm = new HashMap<SEARCH_MODES, String>();
+        ArrayList<Smartphone> result = null;
+        while (true) {
+            System.out.println("Insert desired search term:");
+            System.out.println("1 - Search by brand");
+            System.out.println("2 - Search by brand and model");
+            System.out.println("3 - Search by name");
+            System.out.println("4 - Search by price range");
+            System.out.println("0 - Exit");
+            Scanner scanner = new Scanner(System.in);
+            int option = Integer.parseInt(scanner.nextLine());
+            switch (option) {
+                case 0:
+                    System.out.println("Exiting.");
+                    System.exit(0);
+                    break;
+                case 1:
+                    System.out.println("Insert brand:");
+                    String brand = scanner.nextLine();
+                    searchTerm.put(SEARCH_MODES.MARCA, brand);
+                    result = this.requestInfo(searchTerm);
+                    if(result != null) {
+                        System.out.println("The following smartphones have been found:");
+                        for(Smartphone smartphone : result) {
+                            System.out.println("Name: "+smartphone.getName());
+                            System.out.println("Brand: "+smartphone.getBrand());
+                            System.out.println("Price: "+smartphone.getPrice());
+                            System.out.println();
+                        }
+                    }
+                    searchTerm.clear();
+                    break;
+                case 2:
+                    System.out.println("Insert brand and model as \"<brand> <model>\":");
+                    String brand_model = scanner.nextLine();
+                    searchTerm.put(SEARCH_MODES.MARCA_MODELO, brand_model);
+                    result = this.requestInfo(searchTerm);
+                    if(result != null) {
+                        System.out.println("The following smartphones have been found:");
+                        for(Smartphone smartphone : result) {
+                            System.out.println("Name: "+smartphone.getName());
+                            System.out.println("Brand: "+smartphone.getBrand());
+                            System.out.println("Price: "+smartphone.getPrice());
+                            System.out.println();
+                        }
+                    }
+                    searchTerm.clear();
+                    break;
+                case 3:
+                    System.out.println("Insert name:");
+                    String name = scanner.nextLine();
+                    searchTerm.put(SEARCH_MODES.NOME, name);
+                    result = this.requestInfo(searchTerm);
+                    if(result != null) {
+                        System.out.println("The following smartphones have been found:");
+                        for(Smartphone smartphone : result) {
+                            System.out.println("Name: "+smartphone.getName());
+                            System.out.println("Brand: "+smartphone.getBrand());
+                            System.out.println("Price: "+smartphone.getPrice());
+                            System.out.println();
+                        }
+                    }
+                    searchTerm.clear();
+                    break;
+                case 4:
+                    System.out.println("Insert minimum and maximum price as \"<min_price>[-]<max_price>\":");
+                    String prices = scanner.nextLine();
+                    searchTerm.put(SEARCH_MODES.INTERVALO_PRECOS, prices);
+                    result = this.requestInfo(searchTerm);
+                    if(result != null) {
+                        System.out.println("The following smartphones have been found:");
+                        for(Smartphone smartphone : result) {
+                            System.out.println("Name: "+smartphone.getName());
+                            System.out.println("Brand: "+smartphone.getBrand());
+                            System.out.println("Price: "+smartphone.getPrice());
+                            System.out.println();
+                        }
+                    }
+                    searchTerm.clear();
+                    break;
+                default:
+                    System.out.println("Insert a valid option.");
+                    break;
+            }
+        }
+    }
+}
