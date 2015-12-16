60d91fe6acf3ecb336223f77dd205931f761bac7
António Lima <tocalagarto93@gmail.com>
2015-12-16 21:49:34 +0000

    - automatic counting of insertions to smartphones;

--- Assignment3/assignment3/database_files/create_database.sql
+++ Assignment3/assignment3/database_files/create_database.sql
@@ -12,19 +12,26 @@ FLUSH PRIVILEGES;
 
 USE mailinglist;
 
+
+/* Table for Subscriptions to the mailing list */
 CREATE TABLE subscriptions (
 	id mediumint(8) unsigned NOT NULL auto_increment,
+
 	email VARCHAR(254) NOT NULL, /* as per RFC 5321 */
 	name VARCHAR(50) NOT NULL,
 	brand_filter VARCHAR(50) NOT NULL,
 	min_price INT NOT NULL,
 	max_price INT NOT NULL,
 	is_validated TINYINT(1) unsigned NOT NULL DEFAULT 0,
+
 	PRIMARY KEY (id)
 ) AUTO_INCREMENT=1;
 
+
+/* Table for Smartphones */
 CREATE TABLE smartphones (
 	id mediumint(8) unsigned NOT NULL auto_increment,
+
 	technical_data TEXT,
     url VARCHAR(256) NOT NULL,
 	name VARCHAR(50) NOT NULL,
@@ -32,10 +39,45 @@ CREATE TABLE smartphones (
 	currency VARCHAR(50) NOT NULL,
 	summary_data TEXT NOT NULL,
 	price DECIMAL NOT NULL,
+
 	PRIMARY KEY (id)
 ) AUTO_INCREMENT=1;
 
 
+/* Table for System Statistics */
+CREATE TABLE statistics (
+	num_updates_to_smartphones INT DEFAULT 0,
+	num_total_smartphones INT DEFAULT 0,
+	num_sent_emails INT DEFAULT 0,
+	num_sent_tweets INT DEFAULT 0
+);
 
-/* clear console */
-system clear;
+
+/* automatic incrementing of smartphone update counter and total number of */
+DELIMITER $$
+CREATE TRIGGER before_smartphone_insert 
+    BEFORE INSERT ON smartphones
+    FOR EACH ROW
+BEGIN
+    UPDATE statistics
+    SET num_updates_to_smartphones = num_updates_to_smartphones + 1; 
+END $$
+DELIMITER ;
+
+
+/* Initialize Statistics Counters */
+INSERT INTO statistics (num_updates_to_smartphones, num_total_smartphones, num_sent_emails, num_sent_tweets) VALUES (0,0,0,0);
+
+
+/* For debugging purposes */
+INSERT INTO smartphones (technical_data, url, name, brand, currency, summary_data, price) VALUES ('Some technical mumbo jumbo','www.my.url.com','Generic Smartphone','Generic Brand','€','My summary is bigger than yours!', 1337);
+
+
+
+
+
+
+
+
+
+
--- Assignment3/assignment3/database_files/run_me.sh
+++ Assignment3/assignment3/database_files/run_me.sh
@@ -5,4 +5,6 @@ mysql.server start
 
 # the password is the empty string	
 mysql -u root -D mysql -p
-source /Users/aclima/Documents/Repositories/Systems_Integration/Assignment3/assignment3/database_files/create_database.sql;
+
+# do this afterwards
+# mysql> source /Users/aclima/Documents/Repositories/Systems_Integration/Assignment3/assignment3/database_files/create_database.sql;
