bdfbabdb8204f49f2bb4f29ad5b8f45789a2ae77
António Lima <tocalagarto93@gmail.com>
2015-12-17 20:23:35 +0000

    - adding files for XML to Objects conversion -- did not work but i'm betting it will still be useful in the future

---
+++ Assignment3/assignment3/src/main/java/ObjectFactory.java
@@ -0,0 +1,74 @@
+
+package generated;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the generated package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: generated
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link List<Smartphone> }
+     * 
+     */
+    public List<Smartphone> createSmartphones() {
+        return new ArrayList<Smartphone>();
+    }
+
+    /**
+     * Create an instance of {@link Smartphone }
+     * 
+     */
+    public Smartphone createSmartphone() {
+        return new Smartphone();
+    }
+
+    /**
+     * Create an instance of {@link Smartphone.TechnicalData }
+     * 
+     */
+    public Smartphone.TechnicalData createSmartphoneTechnicalData() {
+        return new Smartphone.TechnicalData();
+    }
+
+    /**
+     * Create an instance of {@link Smartphone.TechnicalData.Table }
+     * 
+     */
+    public Smartphone.TechnicalData.Table createSmartphoneTechnicalDataTable() {
+        return new Smartphone.TechnicalData.Table();
+    }
+
+    /**
+     * Create an instance of {@link Smartphone.TechnicalData.Table.TableData }
+     * 
+     */
+    public Smartphone.TechnicalData.Table.TableData createSmartphoneTechnicalDataTableTableData() {
+        return new Smartphone.TechnicalData.Table.TableData();
+    }
+
+}
