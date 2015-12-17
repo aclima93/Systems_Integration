7fcd4658fe9009b05637ba0476d7e60a7bb5f46e
António Lima <tocalagarto93@gmail.com>
2015-12-17 22:45:41 +0000

    - updated database run_me.sh - added unsubscribe_flow

--- Assignment3/assignment3/database_files/run_me.sh
+++ Assignment3/assignment3/database_files/run_me.sh
@@ -4,7 +4,4 @@ mysql.server stop
 mysql.server start
 
 # the password is the empty string	
-mysql -u root -D mysql -p
-
-# do this afterwards
-# mysql> source /Users/aclima/Documents/Repositories/Systems_Integration/Assignment3/assignment3/database_files/create_database.sql;
+mysql -u root -D mysql -p < create_database.sql
