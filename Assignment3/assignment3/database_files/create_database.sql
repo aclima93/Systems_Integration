d0a121156b509290523dd77b693429fe8efc8aaf
António Lima <tocalagarto93@gmail.com>
2015-12-20 19:08:53 +0000

    - the triggers on the database side were messing up everything, so now we have to do what they would automagically do on mule -- mule is, without a doubt, a wonderful tool. except when it's not.

--- Assignment3/assignment3/database_files/create_database.sql
+++ Assignment3/assignment3/database_files/create_database.sql
@@ -5,10 +5,9 @@ CREATE DATABASE mailinglist;
 DROP USER generatedata@localhost;
 FLUSH PRIVILEGES;
 CREATE USER generatedata@localhost IDENTIFIED BY 'generatedata';
-*/
-
 GRANT ALL PRIVILEGES ON mailinglist.* TO generatedata@localhost IDENTIFIED BY 'generatedata';
 FLUSH PRIVILEGES;
+*/
 
 USE mailinglist;
 
@@ -53,72 +52,5 @@ CREATE TABLE statistics (
 );
 
 
-/* Automatic incrementing of smartphone insertions counter*/
-DELIMITER $$
-CREATE PROCEDURE update_smartphone_statistics()
-BEGIN
-    UPDATE statistics
-    SET num_updates_to_smartphones = num_updates_to_smartphones + 1,
-    	num_cur_smartphones = (SELECT COUNT(*) FROM smartphones);
-END$$
-DELIMITER ;
-
-DELIMITER $$
-CREATE TRIGGER after_smartphone_insert 
-    AFTER INSERT ON smartphones
-    FOR EACH ROW
-BEGIN
-    CALL update_smartphone_statistics();
-END$$
-DELIMITER ;
-
-DELIMITER $$
-CREATE TRIGGER after_smartphone_update 
-    AFTER UPDATE ON smartphones
-    FOR EACH ROW
-BEGIN
-    CALL update_smartphone_statistics();
-END$$
-DELIMITER ;
-
-DELIMITER $$
-CREATE TRIGGER after_smartphone_delete
-    AFTER DELETE ON smartphones
-    FOR EACH ROW
-BEGIN
-    CALL update_smartphone_statistics();
-END$$
-DELIMITER ;
-
-
 /* Initialize Statistics Counters */
 INSERT INTO statistics () VALUES ();
-
-
-/* For debugging purposes */
-/* before any action */
-/*SELECT * FROM statistics;
-
-/* after insert */
-/*INSERT INTO smartphones (technical_data, url, name, brand, currency, summary_data, price) VALUES ('Some technical mumbo jumbo','www.my.url.com','Generic Smartphone','Generic Brand','€','My summary is bigger than yours!', 1337);
-/*SELECT * FROM statistics;
-/*SELECT * FROM smartphones;*/
-
-/* after update */
-/*UPDATE smartphones SET price = 666 WHERE id = 1;
-/*SELECT * FROM statistics;
-/*SELECT * FROM smartphones;*/
-
-/* after delete */
-/*DELETE FROM smartphones WHERE id = 1;
-/*SELECT * FROM statistics;
-/*SELECT * FROM smartphones;*/
-
-
-
-
-
-
-
-
-
