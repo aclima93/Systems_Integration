87366a69e8373798abc577507dda0e01d775268b
Pedro Janeiro <pjaneiro@student.dei.uc.pt>
2015-12-22 23:37:52 +0000

    Web interface done

---
+++ Assignment3/assignment3/src/main/java/main/SOAP_Client.java
@@ -0,0 +1,88 @@
+package main;
+
+import java.util.Scanner;
+
+import add_smartphone_artifact.SmartphoneService;
+import add_smartphone_artifact.Smartphone;
+import add_subscription_artifact.SubscriptionService;
+import add_subscription_artifact.Subscription;
+import unsubscribe_artifact.UnsubscriptionService;
+import unsubscribe_artifact.Unsubscription;
+
+public class SOAP_Client {
+
+	public static void main(String[] args) {
+		int option = 0;
+		Scanner sc = new Scanner(System.in);
+		do {
+			try {
+				System.out.println("Insert wanted option: ");
+				System.out.println("1 - Add subscription");
+				System.out.println("2 - Delete subscription");
+				System.out.println("3 - Add smartphone");
+				option = Integer.parseInt(sc.nextLine());
+			} catch(NumberFormatException e) {}
+			
+		} while(option < 1 || option > 3);
+
+		switch(option) {
+			case 1: {
+				String email, name, brand, min_price, max_price;
+				System.out.println("Insert your e-mail: ");
+				email = sc.nextLine();
+				System.out.println("Insert smartphone name: ");
+				name = sc.nextLine();
+				System.out.println("Insert smartphone brand: ");
+				brand = sc.nextLine();
+				System.out.println("Insert minimum price: ");
+				min_price = sc.nextLine();
+				System.out.println("Insert maximum price: ");
+				max_price = sc.nextLine();
+				subscribe(email, name, brand, min_price, max_price);
+				break;
+			} case 2: {
+				String email;
+				System.out.println("Insert your e-mail: ");
+				email = sc.nextLine();
+				unsubscribe(email);
+				break;
+			} case 3: {
+				String xml;
+				System.out.println("Insert smartphone xml as a single line string: ");
+				xml = sc.nextLine();
+				addSmartphone(xml);
+				break;
+			} default: {
+				System.out.println("Oops, something went wrong!");
+			}
+		}
+
+	}
+
+	public static String subscribe(String email, String name, String brand, String min_price, String max_price) {
+		SubscriptionService as = new SubscriptionService();
+		Subscription asp = as.getSubscriptionPort();
+		
+		String result = asp.setParams(email, name, brand, min_price, max_price);
+
+		return result;
+	}
+
+	public static String unsubscribe(String email) {
+		UnsubscriptionService as = new UnsubscriptionService();
+		Unsubscription asp = as.getUnsubscriptionPort();
+		
+		String result = asp.setParams(email);
+
+		return result;
+	}
+
+	public static String addSmartphone(String xml) {
+		SmartphoneService as = new SmartphoneService();
+		Smartphone asp = as.getSmartphonePort();
+		
+		String result = asp.setXML(xml);
+
+		return result;
+	}
+}
